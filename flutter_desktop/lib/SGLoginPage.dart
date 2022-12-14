dc65d47c700dd168bcc130102eed0bc6f267e14e
SGN SGN <sgnsgn@SGNui-MacBookAir-2.local>
2022-12-14 14:30:21 +0900

    just test commit

--- flutter_desktop/lib/SGLoginPage.dart
+++ flutter_desktop/lib/SGLoginPage.dart
@@ -1,6 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'dart:async';
-//import 'package:desktop_window/desktop_window.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart' as materials;
 import 'package:window_manager/window_manager.dart';
